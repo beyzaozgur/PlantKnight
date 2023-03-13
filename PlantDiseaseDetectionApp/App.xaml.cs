a0b27d3a91b3398c424542ec86556460d91a88eb
beyzaozgur <beyzaozgur35@hotmail.com>
2023-03-13 22:06:03 +0300

    files added

---
+++ PlantDiseaseDetectionApp/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace PlantDiseaseDetectionApp;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new MainPage();
+	}
+}
---
+++ PlantDiseaseDetectionApp/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace PlantDiseaseDetectionApp;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
