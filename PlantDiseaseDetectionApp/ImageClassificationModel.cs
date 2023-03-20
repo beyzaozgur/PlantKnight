35be1580a7350cd14b4b0e4a67988fe036c15509
Emircan Tepe <emircan.tepe@ogr.deu.edu.tr>
2023-03-20 08:58:41 +0300

    add imageClassificationClass

---
+++ PlantDiseaseDetectionApp/ImageClassificationModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlantDiseaseDetectionApp
+{
+    public class ImageClassificationModel
+    {
+        public ImageClassificationModel(string tagName, float probability)
+        {
+            TagName = tagName;
+            Probability = probability;
+        }
+
+        public float Probability { get; }
+        public string TagName { get; }
+    }
+}
