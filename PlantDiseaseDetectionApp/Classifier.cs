727e242fc86d82cf17e5d97a4b43e9c44f7ff562
Emircan Tepe <emircan.tepe@ogr.deu.edu.tr>
2023-03-27 19:56:27 +0300

    delete tensorflow methods & jump to using custom vision API

---
+++ PlantDiseaseDetectionApp/Classifier.cs
@@ -0,0 +1,27 @@
+﻿
+using Microsoft.Cognitive.CustomVision.Prediction;
+using Microsoft.Cognitive.CustomVision.Prediction.Models;
+using Microsoft.Cognitive.CustomVision.Training;
+using Microsoft.Cognitive.CustomVision.Training.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+namespace PlantDiseaseDetectionApp;
+public static class Classifier
+{
+    public static void RunClassifier()
+    {
+        var keys = GetApiKeys();
+        var trainingApi = new TrainingApi { ApiKey = keys.TrainingKey };
+        var projects = trainingApi.GetProjects();
+        var herbProject = projects.FirstOrDefault(p => p.Name == "Herbs");
+        var predictionEndpoint = new PredictionEndpoint { ApiKey = keys.PredictionKey };
+
+
+        var imageFile = File.OpenRead(imagePath);
+        var result = predictionEndpoint.PredictImage(herbProject.Id, imageFile);
+
+    }
+}
